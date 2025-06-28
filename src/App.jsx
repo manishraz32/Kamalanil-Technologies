dc3bb5bb13e92ef32869a8ed73833264efce3672
karishmakumariDev <kumarikarishma311329@gmail.com>
2025-06-28 16:45:51 +0530

    create navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import About from './pages/About'
+import About from'./pages/About/About.jsx';
 export default function App() {
   return (
     <>
