731ae101f932e52eb8513dcab2594fa33bd7af8c
karishmakumariDev <kumarikarishma311329@gmail.com>
2025-07-16 13:43:14 +0530

    some change in footer section

--- src/pages/Fullstack/Fullstack.jsx
+++ src/pages/Fullstack/Fullstack.jsx
@@ -34,7 +34,6 @@ import image131 from "../../assets/image 131.svg";
 import SuccessModal from "../../components/SuccessModal";
 
 const Fullstack = () => {
-  
   const [formData, setFormData] = useState({
     name: "",
     phone: "",
@@ -138,7 +137,6 @@ const Fullstack = () => {
     }
   };
 
-  
   return (
     <>
       <ScrollToTop />
@@ -790,10 +788,10 @@ const Fullstack = () => {
         </div>
       </section>
 
-      <footer className="flex justify-center bg-[#2F3645] px-4 py-10 md:pt-[clamp(10px,3.125vw,45px)] md:pr-[clamp(94px,3.40vw,49px)] pb-20 md:pl-[clamp(94px,8.19vw,236px)]">
-        <div className="gap-8 md:gap-30 grid grid-cols-1 md:grid-cols-3">
+      <footer className="flex justify-center bg-[rgb(47,54,69)] px-4 py-10 md:pt-[clamp(10px,3.125vw,45px)] md:pr-[clamp(94px,3.40vw,49px)] pb-20 md:pl-[clamp(94px,8.19vw,236px)]">
+        <div className="flex md:flex-row flex-col justify-between w-full">
           {/* Logo & Description */}
-          <div className="flex flex-col items-center md:items-start space-y-4 text-center md:text-start">
+          <div className="flex flex-col items-center md:items-start space-y-4 md:w-[25%] text-center md:text-start">
             <div className="flex items-center gap-2">
               <img src={company_logo} alt="Logo" />
             </div>
@@ -824,8 +822,8 @@ const Fullstack = () => {
           </div>
 
           {/* Quick Links */}
-          <div className="flex flex-col items-center gap-4 md:gap-0">
-            <h3 className="font-[Montserrat] font-bold text-[16px] text-white md:text-[clamp(12px,1.38vw,40px)] text-center md:leading-[clamp(50px,4.375vw,126px)]">
+          <div className="flex flex-col items-center md:items-start gap-4 md:gap-0 text-center">
+            <h3 className="md:mb-5 font-[Montserrat] font-bold text-[16px] text-white md:text-[clamp(12px,1.38vw,40px)] text-center">
               Quick Links
             </h3>
             <ul className="space-y-4 font-medium text-[#00FFCA] text-sm">
@@ -844,8 +842,8 @@ const Fullstack = () => {
             </ul>
           </div>
 
-          {/* Contact Info */}
-          <div className="flex flex-col items-center md:items-start space-y-4 text-center md:text-start">
+          {/* Contact Info  */}
+          <div className="flex flex-col items-center md:items-start space-y-4 text-center md:text-center">
             <h3 className="font-[Montserrat] font-bold text-[16px] text-white md:text-[clamp(16px,1.38vw,40px)]">
               CONTACT
             </h3>
@@ -855,7 +853,7 @@ const Fullstack = () => {
                 <div className="flex md:flex-row flex-col items-center md:items-start gap-2">
                   <img
                     src={smallMapIcon}
-                    className="md:mt-2 w-[15px] h-[17px]"
+                    className="md:mt-2 w-[20px] h-[16px]"
                   />
                   <p className="font-bold text-[12px] text-white md:text-[clamp(12px,1.38vw,40px)]">
                     Location
@@ -872,7 +870,7 @@ const Fullstack = () => {
                 <div className="flex md:flex-row flex-col items-center md:items-start gap-2">
                   <img
                     src={smallPhoneSvg}
-                    className="md:mt-2 w-[15px] h-[17px]"
+                    className="md:mt-2 w-[20px] h-[16px]"
                   />
                   <p className="font-bold text-[12px] text-white md:text-[clamp(12px,1.38vw,40px)]">
                     Phone
@@ -889,7 +887,7 @@ const Fullstack = () => {
                 <div className="flex md:flex-row flex-col items-center md:items-start gap-2">
                   <img
                     src={smallEmailSvg}
-                    className="md:mt-2 w-[15px] h-[17px]"
+                    className="md:mt-2 w-[20px] h-[16px]"
                   />
                   <p className="font-bold text-[12px] text-white md:text-[clamp(12px,1.38vw,40px)]">
                     Email
@@ -922,8 +920,7 @@ const Fullstack = () => {
       </footer>
       {showSuccess && <SuccessModal onClose={() => setShowSuccess(false)} />}
     </>
-   
-);
+  );
 };
 
 export default Fullstack;
